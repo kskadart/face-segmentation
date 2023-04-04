abaa12e808f051eec3cb54712147b6e3d965589b
Kirill Konovalov <kirill.konovalov@picsart.com>
2023-04-04 23:34:45 +0300

    feat(webcamera): FS-0001 added camera hook and element

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useCallback, useState} from 'react';
+import WebCameraHook from "./components/WebCameraHook";
 import './App.css';
 
+
 function App() {
+  const [startStream, setStartStream] = useState<boolean>(false);
+  const webCameraCallback = useCallback((event: any) => {
+
+  }, []);
+  const {
+    mediaRecorderStart,
+    mediaRecorderStop,
+    mediaStreamStop,
+    videoRef,
+  } = WebCameraHook({streamRecorderCallback: webCameraCallback, startStream});
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video ref={videoRef}/>
     </div>
   );
 }
